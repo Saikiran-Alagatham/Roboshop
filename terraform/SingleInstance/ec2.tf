9ac8000f5a81dd6a4830a425ef0b0975f211677f
Saikiran-Alagatham <saikiran.ask007@gmail.com>
2021-09-23 00:41:28 +0000

    security group

--- terraform/SingleInstance/ec2.tf
+++ terraform/SingleInstance/ec2.tf
@@ -4,8 +4,9 @@ provider "aws" {
 
 
 resource "aws_instance" "singleServer"{
-    ami            = "ami-074df373d6bafa625"
-    instance_type  = "t2.micro"
+    ami                     = "ami-074df373d6bafa625"
+    instance_type           = "t2.micro"
+    vpc_security_group_ids  = [aws_security_group.allow_ssh_single_server.id]
 
     tag     = {
         name    = "roboshopSingle"
