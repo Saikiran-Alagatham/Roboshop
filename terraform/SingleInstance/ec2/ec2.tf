3c024dddf66cc7b82f430817247f79c0409c50ec
Saikiran-Alagatham <saikiran.ask007@gmail.com>
2021-09-23 03:52:04 +0000

    creating multiple instances

--- terraform/SingleInstance/ec2/ec2.tf
+++ terraform/SingleInstance/ec2/ec2.tf
@@ -1,11 +1,12 @@
 
 resource "aws_instance" "SingleInstance"{
+    count               = 3
     ami                 = "ami-074df373d6bafa625"
     instance_type       = "t2.micro"
     vpc_security_group_ids = [var.sg_id]
 
     tags    = {
-        Name    = "roboshopSingle"
+        Name    = "roboshopSingle-${count.index}"
     }
 }
 
