eb0c9961d0507ce1be94ffb0a89c29524a560a07
Saikiran-Alagatham <saikiran.ask007@gmail.com>
2021-09-23 03:09:41 +0000

    Modules creation

--- terraform/SingleInstance/ec2/ec2.tf
+++ terraform/SingleInstance/ec2/ec2.tf
@@ -2,7 +2,7 @@
 resource "aws_instance" "SingleInstance"{
     ami                 = "ami-074df373d6bafa625"
     instance_type       = "t2.micro"
-    vpc_security_group_ids = [sg_id]
+    vpc_security_group_ids = [var.sg_id]
 
     tags    = {
         name    = "roboshopSingle"
