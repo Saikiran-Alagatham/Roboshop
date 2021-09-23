2c99bcfd1187d85f860d4cc7af952dbc8495d8cb
Saikiran-Alagatham <saikiran.ask007@gmail.com>
2021-09-23 02:14:01 +0000

    SngleWithS3

---
+++ terraform/singleWithS3/ec2.tf
@@ -0,0 +1,41 @@
+provider "aws"{
+    region      = "us-east-1"
+}
+
+resource "aws_instance" "singleS3" {
+    ami                     = "ami"
+    instance_type           = "t2.micro"
+    vpc_security_group_ids  = []
+
+    tags        = {
+        name    = "singleWithS3"
+    }
+}
+
+resource "aws_security_group" "allow_ssh_single_server" {
+    name            = "allow_ssh_single_server"
+    description     = "allow_ssh_single_server"
+    
+    ingress {
+        from_port       = 22
+        to_port         = 22
+        protocol        = "tcp"
+        cidr_blocks     = ["0.0.0.0/0"]
+    }
+
+
+    egress {
+        from_port       = 0
+        to_port         = 0
+        protocol        = "-1"
+        cidr_blocks     = ["0.0.0.0/0"]
+    }
+}
+
+terraform{
+    backend  "s3" {
+        bucket       = "roboshop-s3"
+        key          = "roboshop/terraform.tfstate"
+        region       = "us-east-1"
+    }
+}
