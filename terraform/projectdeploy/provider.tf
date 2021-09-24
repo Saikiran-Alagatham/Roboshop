2fde686390fe7cee517367406d6ce1831d99cf12
Saikiran-Alagatham <saikiran.ask007@gmail.com>
2021-09-24 01:59:31 +0000

    removing s3

--- terraform/projectdeploy/provider.tf
+++ terraform/projectdeploy/provider.tf
@@ -3,11 +3,4 @@ provider "aws" {
 }
 
 
-terraform{
-    backend "s3"{
-        bucket  = "roboshop-s3"
-        key     = "roboshop/projectdeploy/terraform.tfstate"
-        region  = "us-east-1"
-        dynamodb_table = "terraform"
-    }
-}
+
