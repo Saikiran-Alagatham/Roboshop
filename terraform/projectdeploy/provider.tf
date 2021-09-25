4c4ae3ff37f782bb281c008623640c8b4f14fa93
Saikiran-Alagatham <saikiran.ask007@gmail.com>
2021-09-25 03:44:33 +0000

    adding required provider block

--- terraform/projectdeploy/provider.tf
+++ terraform/projectdeploy/provider.tf
@@ -1,13 +1,21 @@
-provider "aws" {
-    region = "us-east-1"
-}
-
-
 terraform{
+    required_providers {
+        aws = {
+            source  =   "hashicorp/aws"
+            version =   " ~> 3.0"
+        }
+    }
+
     backend "s3"{
         bucket  = "roboshop-s3"
         key     = "roboshop/projectdeploy/terraform.tfstate"
         region  = "us-east-1"
         dynamodb_table = "terraform"
     }
+}
+
+
+
+provider "aws" {
+    region = "us-east-1"
 }
