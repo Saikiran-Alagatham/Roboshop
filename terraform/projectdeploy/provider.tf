481b916f938d5508851466fb9eb9dd900266169f
Saikiran-Alagatham <saikiran.ask007@gmail.com>
2021-10-06 18:44:15 +0000

    testing without s3

--- terraform/projectdeploy/provider.tf
+++ terraform/projectdeploy/provider.tf
@@ -1,9 +1,9 @@
 terraform{
     backend "s3"{
-        bucket  = "roboshop-s3"
-        key     = "roboshop/projectdeploy/terraform.tfstate"
-        region  = "us-east-1"
-        dynamodb_table = "terraform"
+        # bucket  = "roboshop-s3"
+        # key     = "roboshop/projectdeploy/terraform.tfstate"
+        # region  = "us-east-1"
+        # dynamodb_table = "terraform"
     }
 }
 
