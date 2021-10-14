1d111e8c40a304a6d111ed6636f55cc484cf861e
Saikiran-Alagatham <saikiran.ask007@gmail.com>
2021-10-14 05:00:02 +0000

    chaniging hosted zone and SG

--- terraform/deploy/ec2.tf
+++ terraform/deploy/ec2.tf
@@ -2,38 +2,19 @@ resource "aws_instance" "cheap_worker" {
   count                     = local.LENGTH
   ami                       = "ami-074df373d6bafa625"
   instance_type             = "t3.micro"
-  vpc_security_group_ids    =  [aws_security_group.allow_ssh_single_server.id]
+  vpc_security_group_ids    =  ["sg-04371d9790f1294b1"]
   tags                      = {
     Name                    = element(var.COMPONENTS, count.index)
   }
 }
 
-# resource "aws_ec2_tag" "name-tag" {
-#   count                     = local.LENGTH
-#   resource_id               = element(aws_instance.cheap_worker.*.public_ip, count.index)
-#   key                       = "Name"
-#   value                     = element(var.COMPONENTS, count.index)
-# }
+ resource "aws_ec2_tag" "name-tag" {
+   count                     = local.LENGTH
+   resource_id               = element(aws_instance.cheap_worker.*.public_ip, count.index)
+   key                       = "Name"
+   value                     = element(var.COMPONENTS, count.index)
+ }
 
-resource "aws_security_group" "allow_ssh_single_server" {
-    name            = "allow_ssh_single_server"
-    description     = "allow_ssh_single_server"
-    
-    ingress {
-        from_port       = 22
-        to_port         = 22
-        protocol        = "tcp"
-        cidr_blocks     = ["0.0.0.0/0"]
-    }
-
-
-    egress {
-        from_port       = 0
-        to_port         = 0
-        protocol        = "-1"
-        cidr_blocks     = ["0.0.0.0/0"]
-    }
-}
 
 
 
@@ -42,7 +23,7 @@ resource "aws_route53_record" "records" {
   count                     = local.LENGTH
   name                      = element(var.COMPONENTS, count.index)
   type                      = "A"
-  zone_id                   = "Z04058843B1ZOXKPWHN2J"
+  zone_id                   = "Z00620242Y7LFBGQOS2W8"
   ttl                       = 300
   records                   = [element(aws_instance.cheap_worker.*.private_ip, count.index)]
 }
